0b4fb0e0c1719a3aa18ab236503050ff3654f31c
Ceasar Barbosa <Ceaz@Ceasars-MacBook-Pro-2.local>
2016-02-16 15:51:21 -0600

    Facebook Login

--- eventBox/NetworkManager.swift
+++ eventBox/NetworkManager.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import Firebase
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class NetworkManager {
     
@@ -26,7 +28,41 @@ class NetworkManager {
     
     let rootRef = Firebase(url: "https://eventbox.firebaseio.com")
     var eventsRef = Firebase!()
-    var authData = FAuthData?()
+    
+    
+    //MARK: Facebook Login
+    let facebookLogin = FBSDKLoginManager()
+    
+    func loginWithFB()
+    {
+        facebookLogin.logInWithReadPermissions(["email"], fromViewController: nil, handler:
+            {(facebookResult, facebookError) -> Void in
+            
+            if facebookError != nil {
+                print("Facebook Login failed. Error \(facebookError)")
+            }
+            else if facebookResult.isCancelled {
+                print("Facebook Login was cancelled")
+            }
+            else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                
+                self.rootRef.authWithOAuthProvider("facebook", token: accessToken, withCompletionBlock: { error, authData in
+                    
+                    if error != nil {
+                        print("Login failed. \(error)")
+                    }
+                    else {
+                        print("Logged In \(authData)")
+                    }
+                
+                })
+            }
+        })
+    }
+    
+    
+    
     
     
 }
